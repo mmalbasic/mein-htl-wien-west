f7ee0fab39090a4825a82d33856c95dc86461f26
mmalbasic <malbasic.m21@htlwienwest.at>
2025-04-08 12:49:44 +0200

    Added privilege enum

--- MeinHTLWienWest/Services/Priviledges.cs
+++ MeinHTLWienWest/Services/Priviledges.cs
@@ -2,9 +2,9 @@
 {
     public enum Priviledges
     {
-        Read_Forum = 1,
-        Write_Forum = 2,
-        Read_Problem = 3,
-        Write_Problem = 4,
+        Admin = 1,
+        Edit,
+        View,
+        Create
     }
 }
